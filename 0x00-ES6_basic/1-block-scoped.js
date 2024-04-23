4fb563fb00ff7bc878ea25c0e43d9d858836c6bb
mojtabababiker <mojmohammad98@gmail.com>
2024-04-23 01:39:26 +0000

    Edit task 1, fix code style eslint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
   if (trueOrFalse) {
     // eslint-disable-next-line
-    let task = true;
+    task = true;
     // eslint-disable-next-line
-    let task2 = false;
+    task2 = false;
   }
 
+  // eslint-disable-next-line
+  var task = false;
+  // eslint-disable-next-line
+  var task2 = true;
   return [task, task2];
 }
