7acf82dc7bd1c05a26a43b1607184212ab1f954c
mojtabababiker <mojmohammad98@gmail.com>
2024-04-25 08:11:17 +0000

    Edit task 3, fix the function return value

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,13 +2,16 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   let message = '';
-  uploadPhoto().then((response) => {
-    message += response.body;
-    return createUser();
-  }).then((response) => {
-    message += ` ${response.firstName} ${response.lastName}`;
-    console.log(message);
-  }).catch(() => {
-    console.log('Signup system offline');
-  });
+  return uploadPhoto()
+    .then((response) => {
+      message += response.body;
+      return createUser();
+    })
+    .then((response) => {
+      message += ` ${response.firstName} ${response.lastName}`;
+      console.log(message);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
