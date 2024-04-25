6362eaae2a595e3040874de9c53ba5301f95b782
mojtabababiker <mojmohammad98@gmail.com>
2024-04-25 08:01:26 +0000

    Edit task 2, fix the return value of the function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,13 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
+  return promise
+    .then(() => {
+      const result = { status: 200, body: 'success' };
+      return result;
+    })
+    .catch(() => {
+      const err = new Error();
+      return err;
+    })
     .finally(() => {
       console.log('Got a response from the API');
     });
