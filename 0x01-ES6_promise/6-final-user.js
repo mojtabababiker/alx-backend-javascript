88a5d7ee7ddf8de56ceb1ee1ecc9fa63506532e9
mojtabababiker <mojmohammad98@gmail.com>
2024-04-27 01:12:14 +0000

    Edit task 6, fix bugs on return value, project 0x01

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,21 +3,13 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const array = [];
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      for (const result of values) {
-        array.push({
-          status: 'resolved',
-          value: result.status,
-        });
-      }
-      return array;
-    })
-    .catch((err) => {
-      array.push({
-        status: 'rejected',
-        value: err,
-      });
-      return array;
-    });
+  return signUpUser(firstName, lastName).then((result) => {
+    array.push({ status: 'fulfilled', value: result });
+    return uploadPhoto(fileName);
+  }).then((result) => {
+    array.push({ status: 'fulfilled', value: result });
+  }).catch((err) => {
+    array.push({ status: 'rejected', value: err });
+  })
+    .finally(() => (array));
 }
