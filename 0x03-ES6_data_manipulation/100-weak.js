fd185a1c1f2622992f2332404e052932cde3aaf3
mojtabababiker <mojmohammad98@gmail.com>
2024-04-30 01:19:47 +0000

    Edit task 100, handle edge case, project 0x03

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,11 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endPoint) {
-  const counter = weakMap.get(endPoint) || 0;
-  if (counter >= 5) {
-    throw new Error('Endpoint load is high');
+  if (endPoint.protocol && endPoint.name) {
+    const counter = weakMap.get(endPoint) || 0;
+    if (counter >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endPoint, counter + 1);
   }
-  weakMap.set(endPoint, counter + 1);
 }
