f2caead852b09b857147eb394f1b566ee230b62c
mojtabababiker <mojmohammad98@gmail.com>
2024-04-30 01:31:26 +0000

    Edit task 100, test some checker requirments, project 0x03

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,10 +2,10 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endPoint) {
   if (endPoint.protocol && endPoint.name) {
-    const counter = weakMap.get(endPoint) || 0;
-    if (counter >= 5) {
+    if (weakMap.get(endPoint) >= 5) {
       throw new Error('Endpoint load is high');
     }
-    weakMap.set(endPoint, counter + 1);
+    const counter = weakMap.get(endPoint) + 1 || 1;
+    weakMap.set(endPoint, counter);
   }
 }
