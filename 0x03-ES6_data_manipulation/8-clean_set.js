377f2100ad9724f3c055efdb6ca443d4405c04e7
mojtabababiker <mojmohammad98@gmail.com>
2024-04-30 00:41:09 +0000

    Edit task 8, trying some solution, fix typo

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,17 @@
 export default function cleanSet(set, startString) {
   const result = [];
 
-  if (startString.length = 0 || !(set instanceof Set)) {
+  if (startString.length === 0 || !(set instanceof Set)) {
     return '';
   }
+
   set.forEach((val) => {
     if (val.startsWith(startString)) {
       result.push(val.slice(startString.length));
     }
   });
-  if (result.length = 0) {
+
+  if (result.length === 0) {
     return '';
   }
   return result.join('-');
