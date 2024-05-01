672b078df1be860e61c2dfdbf0382077c4b231a0
mojtabababiker <mojmohammad98@gmail.com>
2024-05-01 22:18:06 +0000

    Edit task 100, fix bug on traking number of queries, project 0x03

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,11 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endPoint) {
+  const counter = weakMap.get(endPoint) + 1 || 1;
+  weakMap.set(endPoint, counter);
   if (endPoint.protocol && endPoint.name) {
     if (weakMap.get(endPoint) >= 5) {
       throw new Error('Endpoint load is high');
     }
-    const counter = weakMap.get(endPoint) + 1 || 1;
-    weakMap.set(endPoint, counter);
   }
 }
