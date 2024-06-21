722febd304d28b6b41cb34b428ee7915ae2e049a
mojtabababiker <mojmohammad98@gmail.com>
2024-06-21 07:11:22 +0200

    Edit task 6 and 7, edit code style, project 0x05

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('Hello Holberton School!');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,47 +1,48 @@
 // building simple app server with express.js
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('Hello Holberton School!');
+  res.end('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    res.write('This is the list of our students');
-    const path = process.argv[2];
-    res.write('This is the list of our students');
-    fs.readFile(path, 'utf-8', (error, data) => {
-        if (error) {
-            res.end('\nCannot load the database');
-            return;
-        }
-        let studentNum = 0;
-        const fieldStudent = {};
+  res.write('This is the list of our students');
+  const path = process.argv[2];
+  res.write('This is the list of our students');
+  fs.readFile(path, 'utf-8', (error, data) => {
+    if (error) {
+      res.end('\nCannot load the database');
+      return;
+    }
+    let studentNum = 0;
+    const fieldStudent = {};
 
-        for (const line of data.split('\n').slice(1)) {
-            if (line.length) {
-                const temp = line.split(',');
-                const firstName = temp[0];
-                const field = temp[3];
-                if (field in fieldStudent) {
-                    fieldStudent[field].push(firstName);
-                } else {
-                    fieldStudent[field] = [firstName];
-                }
-                studentNum += 1;
-            }
+    for (const line of data.split('\n').slice(1)) {
+      if (line.length) {
+        const temp = line.split(',');
+        const firstName = temp[0];
+        const field = temp[3];
+        if (field in fieldStudent) {
+          fieldStudent[field].push(firstName);
+        } else {
+          fieldStudent[field] = [firstName];
         }
-        res.write(`\nNumber of students: ${studentNum}`);
-        for (const field in fieldStudent) {
-            if (Object.prototype.hasOwnProperty.call(fieldStudent, field)) {
-                const students = fieldStudent[field];
-                res.write(`\nNumber of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-            }
-        }
-        res.end();
-    });
-})
+        studentNum += 1;
+      }
+    }
+    res.write(`\nNumber of students: ${studentNum}`);
+    for (const field in fieldStudent) {
+      if (Object.prototype.hasOwnProperty.call(fieldStudent, field)) {
+        const students = fieldStudent[field];
+        res.write(`\nNumber of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+      }
+    }
+    res.end();
+  });
+});
 
 app.listen(1245);
 
